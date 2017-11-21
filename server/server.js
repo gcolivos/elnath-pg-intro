427943c389f1d828777856296ddf50d0c2bf9b5a
Guillermo Olivos <gcolivos@gmail.com>
2017-11-21 10:17:16 -0600

    add basic server structure

---
+++ server/server.js
@@ -0,0 +1,14 @@
+var express = require ('express');
+var pg = require ('pg');
+var app = express();
+var bodyParser = require('body-parser');
+
+var app = express();
+var port = 5000;
+
+app.use(bodyParser.urlencoded({extended: true});
+app.use(express.static('server/public'));
+
+app.listen(port, function(){
+    console.log('server is listening on port', port);
+});
