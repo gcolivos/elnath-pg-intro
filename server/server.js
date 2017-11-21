fd099d0affb55dcefd91e2ee042582a61719b7b1
Guillermo Olivos <gcolivos@gmail.com>
2017-11-21 10:25:46 -0600

    Sending hard coded array on shoes get request

--- server/server.js
+++ server/server.js
@@ -6,8 +6,16 @@ var bodyParser = require('body-parser');
 var app = express();
 var port = 5000;
 
-app.use(bodyParser.urlencoded({extended: true});
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static('server/public'));
+
+var shoes = [{name: 'nike', cost: '75'}];
+
+// for localhost:5000/shoes should return array of shoe objects
+
+app.get('/shoes', function(req, res){
+    res.send(shoes);
+});
 
 app.listen(port, function(){
     console.log('server is listening on port', port);
